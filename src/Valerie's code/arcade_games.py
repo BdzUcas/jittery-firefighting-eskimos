a4d3a6f7598eb2ec29dd640682c620ba5817f1b3
Valerie5801 <valerie.yang@ucas-edu.net>
2026-04-27 16:41:08 +0000

    started arcade games

---
+++ src/Valerie's code/arcade_games.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+
+root = tk.Tk()
+
+#pong
+def pong(root):
+    root.title("Pongers")
+    root.configure(background = "black")
+    root.resizeable(False, False)
+    root.geometry("1000x770+250-50")
+
+#matching cards
+def match_cards(root):
+    pass
+
+#rock paper scissors
+def rps(root):
+    pass
