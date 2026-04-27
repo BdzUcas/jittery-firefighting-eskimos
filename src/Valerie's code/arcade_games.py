4ffea0324156e26871ba8544286b8e6d869a9e81
Valerie5801 <valerie.yang@ucas-edu.net>
2026-04-27 10:50:27 -0600

    tried starting pong

--- src/Valerie's code/arcade_games.py
+++ src/Valerie's code/arcade_games.py
@@ -8,6 +8,24 @@ def pong(root):
     root.configure(background = "black")
     root.resizeable(False, False)
     root.geometry("1000x770+250-50")
+    root.attributes("-topmost", True)
+
+    class Ball:
+        def __init__(self, canvas, width, velocity, windowwidth, windowheight):
+            self.canvas = canvas
+            self.width = width
+            self.velocity = velocity
+            self.windowwidth = windowwidth
+            self.windowheight = windowheight
+            #center ball
+            self.centerx = windowwidth/2 - width/2
+            self.centery = windowheight/2 - width/2
+            self.vx = velocity
+            self.vy = velocity
+            self.make_ball = self.canvas.create_oval()
+
+    class Paddle:
+        pass
 
 #matching cards
 def match_cards(root):
