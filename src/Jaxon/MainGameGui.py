8881ab2deff1f72f88f834d7fe9e62338dc5732b
jcaps-ops <jaxon.castro@ucas-edu.net>
2026-04-28 17:45:57 +0000

    t

--- src/Jaxon/MainGameGui.py
+++ src/Jaxon/MainGameGui.py
@@ -6,13 +6,13 @@ root.title("IDK MAN")
 
 
 root.configure(background="light blue")
-root.minsize(2700,2000)
-root.maxsize(2700,2000)
+root.minsize(2600,1200)
+root.maxsize(2600,1200)
 
 #button.pack(pady=20)
 itemframe = Image.open("Images/ItemFrame.png.png")
 itemframe = ImageTk.PhotoImage(itemframe)
 
 itemframe_btn = tk.Button(root, image=itemframe)
-itemframe_btn.pack(padx=1,pady=95)
+itemframe_btn.place(relx=.5, rely=0.90, anchor="n")
 root.mainloop()
